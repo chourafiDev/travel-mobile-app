899ac4e86652ff3d89f9d219f28710f3e12f1db1
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-11 17:10:26 +0100

    refactor: add welcome screen

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HOME, LOGIN, REGISTER } from "../constants/routes";
+import { HOME, LOGIN, REGISTER, WELCOME } from "../constants/routes";
 
 // Screens
 import LoginScreen from "../screens/auth/LoginScreen";
 import RegisterScreen from "../screens/auth/RegisterScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
 import DrawerNavigator from "./DrawerNavigator";
 
 const Stack = createNativeStackNavigator();
@@ -13,8 +14,9 @@ export default function AuthNavigator() {
   return (
     <Stack.Navigator
       initialRouteName={HOME}
-      //   screenOptions={{ headerShown: false }}
+      screenOptions={{ headerShown: false }}
     >
+      <Stack.Screen name={WELCOME} component={WelcomeScreen} />
       <Stack.Screen name={LOGIN} component={LoginScreen} />
       <Stack.Screen name={REGISTER} component={RegisterScreen} />
       <Stack.Screen
