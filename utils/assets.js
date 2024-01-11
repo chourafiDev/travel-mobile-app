ac7bd3bffaa49b891100dfb61c5418562bb55f4f
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-12 00:09:12 +0100

    styles: add new images

--- utils/assets.js
+++ utils/assets.js
@@ -2,6 +2,7 @@
 const welcome = require("../assets/images//welcome.jpg");
 const user = require("../assets/images/user.jpg");
 const bg1 = require("../assets/images/bg1.png");
+const bg2 = require("../assets/images/bg2.png");
 const beach = require("../assets/images/icons/beach.png");
 const camp = require("../assets/images/icons/camp.png");
 const forest = require("../assets/images/icons/forest.png");
@@ -12,11 +13,14 @@ const lake = require("../assets/images/icons/lake.png");
 const mountain = require("../assets/images/icons/mountain.png");
 const ocean = require("../assets/images/icons/ocean.png");
 const sunset = require("../assets/images/icons/sunset.png");
+const light = require("../assets/images/light.png");
+const filter = require("../assets/images/icons/filter.png");
 
 export {
   welcome,
   user,
   bg1,
+  bg2,
   beach,
   camp,
   forest,
@@ -27,4 +31,6 @@ export {
   mountain,
   ocean,
   sunset,
+  light,
+  filter,
 };
