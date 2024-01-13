c24ddcd53d8cd98a758f60b7344e01a1e44c3c34
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-13 09:48:08 +0100

    refactor: add user images

--- utils/assets.js
+++ utils/assets.js
@@ -15,6 +15,11 @@ const ocean = require("../assets/images/icons/ocean.png");
 const sunset = require("../assets/images/icons/sunset.png");
 const light = require("../assets/images/light.png");
 const filter = require("../assets/images/icons/filter.png");
+const user04 = require("../assets/images/04.jpg");
+const user05 = require("../assets/images/04.png");
+const user06 = require("../assets/images/05.png");
+const user07 = require("../assets/images/06.jpg");
+const user08 = require("../assets/images/07.jpg");
 
 export {
   welcome,
@@ -33,4 +38,9 @@ export {
   sunset,
   light,
   filter,
+  user04,
+  user05,
+  user06,
+  user07,
+  user08,
 };
