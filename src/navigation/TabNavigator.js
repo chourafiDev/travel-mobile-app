bf78a67b58cd12ec90a1d4aeca9f8b24f019f216
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-15 17:46:45 +0100

    styles: fix design

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,219 +1,186 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DESTINATIONS, FAVORITES, HOME, SETTING } from "../constants/routes";
-import Icon from "react-native-vector-icons/Feather";
-import { useRef } from "react";
 import { View } from "react-native";
-import { Animated, Dimensions } from "react-native";
 import { useColorScheme } from "nativewind";
+
+import IconFeather from "react-native-vector-icons/Feather";
+import IconFoundation from "react-native-vector-icons/Foundation";
 
 // Screens
 import HomeScreen from "../screens/app/HomeScreen";
 import DestinationsScreen from "../screens/app/DestinationsScreen";
 import FavoritesScreen from "../screens/app/FavoritesScreen";
 import SettingScreen from "../screens/app/SettingScreen";
+import { LinearGradient } from "expo-linear-gradient";
+import { shadow } from "../../utils/theme";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   const { colorScheme } = useColorScheme();
 
-  // Animated Tab Indicator...
-  const tabOffsetValue = useRef(new Animated.Value(0)).current;
-
   return (
-    <>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            // position: "absolute",
-            // bottom: 10,
-            // marginHorizontal: 15,
-            // borderRadius: 10,
-            // borderTopLeftRadius: 20,
-            // borderTopRightRadius: 20,
-            height: 65,
-            borderWidth: 0,
-            borderColor: colorScheme == "light" ? "#ffffff" : "#1A2138",
-            backgroundColor: colorScheme == "light" ? "#ffffff" : "#1A2138",
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          // position: "absolute",
+          // bottom: 10,
+          // marginHorizontal: 15,
+          // borderRadius: 10,
+          // borderTopLeftRadius: 20,
+          // borderTopRightRadius: 20,
+          height: 65,
+          borderWidth: 0,
+          borderColor: colorScheme == "light" ? "#ffffff" : "#1A2138",
+          backgroundColor: colorScheme == "light" ? "#ffffff" : "#1A2138",
 
-            // shadow
-            shadowColor: "#cad2c5",
-            shadowOffset: {
-              width: 6,
-              height: 6,
-            },
-            shadowOpacity: 0.5,
-            shadowRadius: 10,
-
-            elevation: 10,
+          // shadow
+          shadowColor: "#cad2c5",
+          shadowOffset: {
+            width: 6,
+            height: 6,
           },
-        }}
-      >
-        <Tab.Screen
-          name={HOME}
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  // centring Tab Button...
-                  position: "absolute",
-                  top: 18,
-                }}
-              >
-                <Icon
-                  name="home"
-                  size={20}
-                  color={
-                    focused
-                      ? "#23A892"
-                      : colorScheme == "light"
-                      ? "#222B45"
-                      : "#FBFBFB"
-                  }
-                ></Icon>
-              </View>
-            ),
-          }}
-          listeners={({ navigation, route }) => ({
-            // Onpress Update....
-            tabPress: (e) => {
-              Animated.spring(tabOffsetValue, {
-                toValue: 0,
-                useNativeDriver: true,
-              }).start();
-            },
-          })}
-        />
-        <Tab.Screen
-          name={DESTINATIONS}
-          component={DestinationsScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  // centring Tab Button...
-                  position: "absolute",
-                  top: 18,
-                }}
-              >
-                <Icon
-                  name="map"
-                  size={20}
-                  color={
-                    focused
-                      ? "#23A892"
-                      : colorScheme == "light"
-                      ? "#222B45"
-                      : "#FBFBFB"
-                  }
-                ></Icon>
-              </View>
-            ),
-          }}
-          listeners={({ navigation, route }) => ({
-            // Onpress Update....
-            tabPress: (e) => {
-              Animated.spring(tabOffsetValue, {
-                toValue: getWidth() * 1.57,
-                useNativeDriver: true,
-              }).start();
-            },
-          })}
-        />
-        <Tab.Screen
-          name={FAVORITES}
-          component={FavoritesScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  // centring Tab Button...
-                  position: "absolute",
-                  top: 18,
-                }}
-              >
-                <Icon
-                  name="heart"
-                  size={20}
-                  color={
-                    focused
-                      ? "#23A892"
-                      : colorScheme == "light"
-                      ? "#222B45"
-                      : "#FBFBFB"
-                  }
-                ></Icon>
-              </View>
-            ),
-          }}
-          listeners={({ navigation, route }) => ({
-            // Onpress Update....
-            tabPress: (e) => {
-              Animated.spring(tabOffsetValue, {
-                toValue: getWidth() * 3.12,
-                useNativeDriver: true,
-              }).start();
-            },
-          })}
-        />
-        <Tab.Screen
-          name={SETTING}
-          component={SettingScreen}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  // centring Tab Button...
-                  position: "absolute",
-                  top: 18,
-                }}
-              >
-                <Icon
-                  name="settings"
-                  size={20}
-                  color={
-                    focused
-                      ? "#23A892"
-                      : colorScheme == "light"
-                      ? "#222B45"
-                      : "#FBFBFB"
-                  }
-                ></Icon>
-              </View>
-            ),
-          }}
-          listeners={({ navigation, route }) => ({
-            // Onpress Update....
-            tabPress: (e) => {
-              Animated.spring(tabOffsetValue, {
-                toValue: getWidth() * 4.7,
-                useNativeDriver: true,
-              }).start();
-            },
-          })}
-        />
-      </Tab.Navigator>
+          shadowOpacity: 0.5,
+          shadowRadius: 10,
 
-      <Animated.View
-        className="absolute bg-brand h-[2px] bottom-[64px] left-[28px] rounded-full"
-        style={{
-          width: getWidth() - 20,
-          transform: [{ translateX: tabOffsetValue }],
+          elevation: 10,
+        },
+      }}
+    >
+      <Tab.Screen
+        name={HOME}
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                // centring Tab Button...
+                position: "absolute",
+                top: 18,
+              }}
+            >
+              <IconFeather
+                name="home"
+                size={20}
+                color={
+                  focused
+                    ? "#23A892"
+                    : colorScheme == "light"
+                    ? "#222B45"
+                    : "#FBFBFB"
+                }
+              ></IconFeather>
+            </View>
+          ),
         }}
-      ></Animated.View>
-    </>
+      />
+      <Tab.Screen
+        name={DESTINATIONS}
+        component={DestinationsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                // centring Tab Button...
+                position: "absolute",
+                top: 18,
+              }}
+            >
+              <IconFeather
+                name="map"
+                size={20}
+                color={
+                  focused
+                    ? "#23A892"
+                    : colorScheme == "light"
+                    ? "#222B45"
+                    : "#FBFBFB"
+                }
+              ></IconFeather>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="globe"
+        component={DestinationsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={[colorScheme == "light" && shadow.boxShadow]}
+              className="rounded-full overflow-hidden w-[66px] h-[66px] mb-12 border-4 border-white justify-center items-center"
+            >
+              <LinearGradient
+                colors={["#23a892", "#00dbb7"]}
+                className="w-full h-full absolute top-0 right-0"
+                start={{ x: 0.7, y: 0 }}
+                end={{ x: 0.5, y: 1 }}
+              />
+              <IconFeather
+                name="globe"
+                size={23}
+                color={focused ? "#23A892" : "#FBFBFB"}
+              ></IconFeather>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={FAVORITES}
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                // centring Tab Button...
+                position: "absolute",
+                top: 18,
+              }}
+            >
+              <IconFeather
+                name="heart"
+                size={20}
+                color={
+                  focused
+                    ? "#23A892"
+                    : colorScheme == "light"
+                    ? "#222B45"
+                    : "#FBFBFB"
+                }
+              ></IconFeather>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={SETTING}
+        component={SettingScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                // centring Tab Button...
+                position: "absolute",
+                top: 18,
+              }}
+            >
+              <IconFeather
+                name="settings"
+                size={20}
+                color={
+                  focused
+                    ? "#23A892"
+                    : colorScheme == "light"
+                    ? "#222B45"
+                    : "#FBFBFB"
+                }
+              ></IconFeather>
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
-}
-
-function getWidth() {
-  let width = Dimensions.get("window").width;
-
-  // Horizontal Padding = 20...
-  width = width - 80;
-
-  // Total five Tabs...
-  return width / 5;
 }
