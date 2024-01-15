c210915552efb9cbecf71a4ed09452c4ea1c02be
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-15 13:23:12 +0100

    refactor: add notification data

--- utils/data.js
+++ utils/data.js
@@ -59,3 +59,66 @@ export const users = [
     review: "4.0",
   },
 ];
+
+export const notification = [
+  {
+    icon: "map",
+    time: "8:00 AM",
+    date: "May 20",
+    description: "A new destination added",
+  },
+  {
+    icon: "bell",
+    time: "9:34 AM",
+    date: "May 20",
+    description: "A new promo offer is running for Morocco desert",
+  },
+  {
+    icon: "map",
+    time: "9:45 AM",
+    date: "May 21",
+    description: "A new destination added",
+  },
+  {
+    icon: "bell",
+    time: "12:23 AM",
+    date: "May 21",
+    description: "A new promo offer is running for Marrakech tour",
+  },
+  {
+    icon: "map",
+    time: "6:24 AM",
+    date: "May 21",
+    description: "You have booked a new destination successfully",
+  },
+  {
+    icon: "map",
+    time: "20:23 AM",
+    date: "May 22",
+    description: "A new destination added",
+  },
+  {
+    icon: "map",
+    time: "10:12 AM",
+    date: "May 24",
+    description: "A new destination added",
+  },
+  {
+    icon: "map",
+    time: "15:23 AM",
+    date: "May 20",
+    description: "A new destination added",
+  },
+  {
+    icon: "map",
+    time: "11:42 AM",
+    date: "May 24",
+    description: "A new destination added",
+  },
+  {
+    icon: "map",
+    time: "16:13 AM",
+    date: "May 28",
+    description: "A new destination added",
+  },
+];
