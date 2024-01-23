94d11cb583a2876ba519bf3aaa2604edcfe33c2f
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-23 14:58:31 +0100

    style: add onPress prop

---
+++ src/components/ui/OutlineButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const OutlineButton = ({ label, route, size, type, onPress }) => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      className={`rounded-2xl overflow-hidden border ${
+        type == "primary" ? "border-brand" : "border-danger-100"
+      }`}
+      onPress={onPress}
+    >
+      <Text
+        className={`${
+          type == "primary" ? "text-brand" : "text-danger-100"
+        } text-center text-[18px] ${size == "lg" ? "py-3" : "py-1"} `}
+        style={{ fontFamily: "baiJamjuree-bold" }}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default OutlineButton;
