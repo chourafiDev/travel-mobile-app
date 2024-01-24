2042ba714c7287a7f12d3a3755623ffbae090c9c
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-24 16:24:57 +0100

    refactor: add onPress and isLoading state

--- src/components/ui/GradientButton.js
+++ src/components/ui/GradientButton.js
@@ -1,24 +1,15 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Icon from "react-native-vector-icons/Feather";
-import { useNavigation } from "@react-navigation/native";
 
-const GradientButton = ({ label, icon, route, size, type }) => {
-  const navigation = useNavigation();
-
-  const handleNavigate = () => {
-    if (route) {
-      navigation.navigate(route);
-    } else {
-      return;
-    }
-  };
+const GradientButton = ({ label, icon, size, type, onPress, isLoading }) => {
   return (
     <TouchableOpacity
       activeOpacity={0.8}
       className="rounded-2xl overflow-hidden"
-      onPress={handleNavigate}
+      onPress={onPress}
+      disabled={isLoading}
     >
       <LinearGradient
         colors={
@@ -26,26 +17,34 @@ const GradientButton = ({ label, icon, route, size, type }) => {
         }
         className="w-full h-full absolute top-0 right-0"
       />
-      <Text
-        className={`text-white text-center text-[18px] ${
-          size == "lg" ? "py-3" : "py-1"
-        } `}
-        style={{ fontFamily: "baiJamjuree-bold" }}
-      >
-        {label}
-      </Text>
-      {icon && (
-        <Icon
-          name={icon}
-          style={{
-            fontFamily: "baiJamjuree-bold",
-            position: "absolute",
-            right: 20,
-            bottom: "33%",
-          }}
-          size={15}
-          color="#FBFBFB"
-        />
+      {isLoading ? (
+        <View className={size == "lg" ? "py-2" : "py-1"}>
+          <ActivityIndicator size="large" color="#ffffff" />
+        </View>
+      ) : (
+        <>
+          <Text
+            className={`text-white text-center text-[18px] ${
+              size == "lg" ? "py-3" : "py-1"
+            } `}
+            style={{ fontFamily: "baiJamjuree-bold" }}
+          >
+            {label}
+          </Text>
+          {icon && (
+            <Icon
+              name={icon}
+              style={{
+                fontFamily: "baiJamjuree-bold",
+                position: "absolute",
+                right: 20,
+                bottom: "33%",
+              }}
+              size={15}
+              color="#FBFBFB"
+            />
+          )}
+        </>
       )}
     </TouchableOpacity>
   );
