bc24b3f5da0230a265bb09c62facbfb1b6b199f3
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-24 16:23:15 +0100

    refactor: handle get/add/remove from asyn storage

--- utils/asyncStorageService.js
+++ utils/asyncStorageService.js
@@ -1,19 +1,26 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const storeData = async (key, value) => {
-  try {
-    const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem(key, jsonValue);
-  } catch (e) {
-    console.log("async-storage: error store data", e);
-  }
-};
-
 export const getStoredData = async (key) => {
   try {
     const value = await AsyncStorage.getItem(key);
     return value;
   } catch (e) {
     console.log("async-storage: error get stored data", e);
+  }
+};
+
+export const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    console.log("async-storage: error store data", e);
+  }
+};
+
+export const removeStoredData = async (key) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {
+    console.log("async-storage: error remove stored data", e);
   }
 };
