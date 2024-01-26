bd6e8e4d7257890d6c82c71329fce54e59ada599
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-01-26 15:14:32 +0100

    refactor: handle auth

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,24 @@
-import { NavigationContainer } from "@react-navigation/native";
-import AuthNavigator from "./AuthNavigator";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthStackNavigator from "./AuthStackNavigator";
+import AppStackNavigator from "./AppStackNavigator";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { fetchAuthData } from "../store/features/authSlice";
+import { useEffect } from "react";
 
 const AppNavigation = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(fetchAuthData());
+  }, [dispatch]);
+
   return (
     <NavigationContainer>
       <BottomSheetModalProvider>
-        <AuthNavigator />
+        {user ? <AppStackNavigator /> : <AuthStackNavigator />}
       </BottomSheetModalProvider>
     </NavigationContainer>
   );
