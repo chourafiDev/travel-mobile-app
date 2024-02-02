b98e84f834ce7bccf521841c5403d36b8caebcdb
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-02 16:00:53 +0100

    refactor: add home folder

---
+++ src/components/home/Categories.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useGetCategoriesQuery } from "../../store/services/categoriesApiSlice";
+import { ScrollView } from "react-native-gesture-handler";
+import Category from "./Category";
+import Loading from "../Loading";
+
+const Categories = ({ setSelectedCategory, selectedCategory }) => {
+  // fetch categories
+  const { data: categories, isLoading } = useGetCategoriesQuery();
+
+  // select catgeory
+  const handleSelectCategory = (item) => {
+    if (selectedCategory === item) {
+      setSelectedCategory("");
+    } else {
+      setSelectedCategory(item);
+    }
+  };
+
+  return (
+    <View className="px-4 mt-10 space-y-5">
+      <View className="flex-row justify-between items-center">
+        <Text
+          className="text-dark/80 dark:text-white/80 text-[22px]"
+          style={{ fontFamily: "baiJamjuree-semibold" }}
+        >
+          Categories
+        </Text>
+        <TouchableOpacity className="bg-gray-1 dark:bg-dark-2/70  rounded-lg px-3 py-2">
+          <Text
+            className="text-dark/80 text-sm dark:text-white/80"
+            style={{ fontFamily: "baiJamjuree-semibold" }}
+          >
+            View all
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 20,
+        }}
+      >
+        {isLoading ? (
+          <Loading />
+        ) : (
+          categories.map((category) => {
+            return (
+              <Category
+                key={category?.id}
+                category={category}
+                selectedCategory={selectedCategory}
+                handleSelectCategory={handleSelectCategory}
+              />
+            );
+          })
+        )}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Categories;
---
+++ src/components/home/Category.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { shadow } from "../../../utils/theme";
+import { useColorScheme } from "nativewind";
+
+export default function Category({
+  category: { content, imageUrl },
+  selectedCategory,
+  handleSelectCategory,
+}) {
+  const { colorScheme } = useColorScheme();
+  return (
+    <TouchableOpacity
+      className={`rounded-2xl p-1 mr-3 border ${
+        selectedCategory === content
+          ? "bg-brand/10 border-brand/40"
+          : "bg-white dark:bg-dark-2 border-gray-50 dark:border-dark-2"
+      }`}
+      style={[colorScheme == "light" && shadow.boxShadow]}
+      activeOpacity={0.8}
+      onPress={() => handleSelectCategory(content)}
+    >
+      <View className="flex-row items-center gap-2 pr-5">
+        <Image source={{ uri: imageUrl }} className="rounded-xl w-14 h-11" />
+        <Text
+          className={`${
+            selectedCategory === content
+              ? "text-brand"
+              : "text-dark/70 dark:text-white/70"
+          }`}
+          style={{ fontFamily: "baiJamjuree-semibold" }}
+        >
+          {content}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
