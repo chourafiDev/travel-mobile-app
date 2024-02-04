a511a31cd7fb04d544fa55c1d370cceb22aab32c
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-04 23:48:48 +0100

    refactor: add stepper reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
+import stepperReducer from "./features/stepperSlice";
 import { apiSlice } from "./services/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    stepper: stepperReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
 
