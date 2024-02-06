6f06aa48e308748dad391cc23e849d81c4d14c9c
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-06 11:06:49 +0100

    refactor: add AuthForms folder

---
+++ src/components/AuthForms/RegisterForm.js
@@ -0,0 +1,173 @@
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { shadow } from "../../../utils/theme";
+import { useColorScheme } from "nativewind";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { HOME_TAB, LOGIN } from "../../constants/routes";
+import Icon from "react-native-vector-icons/Feather";
+import GradientButton from "../ui/GradientButton";
+import OutlineButton from "../ui/OutlineButton";
+
+const RegisterForm = () => {
+  const { colorScheme } = useColorScheme();
+  const navigation = useNavigation();
+  const [showPassword, setShowPassword] = useState(true);
+
+  return (
+    <View className="px-4 mt-4">
+      {/* Header */}
+      <View className="mb-4">
+        <Text
+          className="text-dark dark:text-white text-[18px]"
+          style={{ fontFamily: "baiJamjuree-medium" }}
+        >
+          Fill your details to <Text className="text-brand">Sign Up</Text>
+        </Text>
+      </View>
+
+      {/* Body */}
+      <Animated.View
+        entering={FadeInDown.duration(1000).springify()}
+        className="w-full flex-row items-center border border-dark/10 px-4 py-3 rounded-2xl bg-white dark:bg-dark-2 mb-3"
+        style={[colorScheme == "light" && shadow.boxShadow]}
+      >
+        <Icon
+          name="user"
+          size={15}
+          color={colorScheme == "light" ? "#222B4580" : "#ffffff"}
+        />
+        <TextInput
+          placeholder="Username"
+          className="text-dark dark:text-white flex-1 ml-3"
+          style={[{ fontFamily: "baiJamjuree-regular" }]}
+          placeholderTextColor={
+            colorScheme == "light" ? "#222B4580" : "#ffffff"
+          }
+        />
+      </Animated.View>
+      <Animated.View
+        entering={FadeInDown.duration(1000).springify()}
+        className="w-full border flex-row items-center border-dark/10 px-4 py-3 rounded-2xl bg-white dark:bg-dark-2 mb-3"
+        style={[colorScheme == "light" && shadow.boxShadow]}
+      >
+        <Icon
+          name="user"
+          size={15}
+          color={colorScheme == "light" ? "#222B4580" : "#ffffff"}
+        />
+        <TextInput
+          placeholder="First Name"
+          className="text-dark dark:text-white flex-1 ml-3"
+          style={[{ fontFamily: "baiJamjuree-regular" }]}
+          placeholderTextColor={
+            colorScheme == "light" ? "#222B4580" : "#ffffff"
+          }
+        />
+      </Animated.View>
+      <Animated.View
+        entering={FadeInDown.duration(1000).springify()}
+        className="w-full border flex-row items-center border-dark/10 px-4 py-3 rounded-2xl bg-white dark:bg-dark-2 mb-3"
+        style={[colorScheme == "light" && shadow.boxShadow]}
+      >
+        <Icon
+          name="user"
+          size={15}
+          color={colorScheme == "light" ? "#222B4580" : "#ffffff"}
+        />
+        <TextInput
+          placeholder="Last Name"
+          className="text-dark dark:text-white flex-1 ml-3"
+          style={[{ fontFamily: "baiJamjuree-regular" }]}
+          placeholderTextColor={
+            colorScheme == "light" ? "#222B4580" : "#ffffff"
+          }
+        />
+      </Animated.View>
+      <Animated.View
+        entering={FadeInDown.duration(1000).springify()}
+        className="w-full border flex-row items-center border-dark/10 px-4 py-3 rounded-2xl bg-white dark:bg-dark-2 mb-3"
+        style={[colorScheme == "light" && shadow.boxShadow]}
+      >
+        <Icon
+          name="mail"
+          size={15}
+          color={colorScheme == "light" ? "#222B4580" : "#ffffff"}
+        />
+        <TextInput
+          placeholder="Email address"
+          className="text-dark dark:text-white flex-1 ml-3"
+          style={[{ fontFamily: "baiJamjuree-regular" }]}
+          placeholderTextColor={
+            colorScheme == "light" ? "#222B4580" : "#ffffff"
+          }
+        />
+      </Animated.View>
+      <Animated.View
+        entering={FadeInDown.delay(200).duration(1000).springify()}
+        className="w-full border flex-row items-center border-dark/10 px-4 py-3 rounded-2xl bg-white dark:bg-dark-2 mb-8"
+        style={[colorScheme == "light" && shadow.boxShadow]}
+      >
+        <Icon
+          name="lock"
+          size={15}
+          color={colorScheme == "light" ? "#222B4580" : "#ffffff"}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={showPassword}
+          className="text-dark dark:text-white flex-1 mx-3"
+          style={[{ fontFamily: "baiJamjuree-regular" }]}
+          placeholderTextColor={
+            colorScheme == "light" ? "#222B4580" : "#ffffff"
+          }
+        />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Icon
+            name={showPassword ? "eye-off" : "eye"}
+            size={17}
+            color={colorScheme == "light" ? "#222B4580" : "#ffffff"}
+          />
+        </TouchableOpacity>
+      </Animated.View>
+
+      {/* Footer */}
+      <Animated.View
+        entering={FadeInDown.delay(400).duration(1000).springify()}
+      >
+        <Animated.View
+          entering={FadeInDown.delay(400).duration(1000).springify()}
+        >
+          <GradientButton
+            label="Sign Up"
+            icon=""
+            size="lg"
+            type="primary"
+            route={HOME_TAB}
+          />
+        </Animated.View>
+      </Animated.View>
+      <Animated.View
+        entering={FadeInDown.delay(600).duration(1000).springify()}
+        className="py-3 flex-row items-center gap-3"
+      >
+        <View className="bg-dark/10 dark:bg-white/10 flex-1 h-[1px]"></View>
+        <Text
+          className="text-dark/40 dark:text-white/40"
+          style={{ fontFamily: "baiJamjuree-medium" }}
+        >
+          Have Account?
+        </Text>
+        <View className="bg-dark/10 dark:bg-white/10 flex-1 h-[1px]"></View>
+      </Animated.View>
+      <Animated.View
+        entering={FadeInDown.delay(600).duration(1000).springify()}
+      >
+        <OutlineButton label="Login" type="primary" size="lg" />
+      </Animated.View>
+    </View>
+  );
+};
+
+export default RegisterForm;
