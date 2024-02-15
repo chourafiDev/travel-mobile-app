3165340886973b2e171921b61d4376b3269cd1f1
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-15 21:29:13 +0100

    refactor: handle profile actions

---
+++ src/store/services/profileApiSlice.js
@@ -0,0 +1,46 @@
+import { apiSlice } from "./apiSlice";
+
+const PROFILE_URL = "profile";
+
+const apiSliceWithTag = apiSlice.enhanceEndpoints({
+  addTagTypes: ["profile"],
+});
+
+export const profileApiSlice = apiSliceWithTag.injectEndpoints({
+  endpoints: (builder) => ({
+    getProfile: builder.query({
+      query: () => {
+        return {
+          url: `${PROFILE_URL}`,
+          method: "GET",
+          credentials: "include",
+        };
+      },
+      providesTags: ["profile"],
+    }),
+    updateProfile: builder.mutation({
+      query: (data) => {
+        return {
+          url: `${PROFILE_URL}`,
+          method: "PUT",
+          body: data,
+          credentials: "include",
+        };
+      },
+      invalidatesTags: ["profile"],
+    }),
+    updateImageProfile: builder.mutation({
+      query: (data) => {
+        return {
+          url: `${PROFILE_URL}/edit-image`,
+          method: "PATCH",
+          body: data,
+          credentials: "include",
+        };
+      },
+      invalidatesTags: ["profile"],
+    }),
+  }),
+});
+
+export const { useGetProfileQuery, useUpdateProfileMutation, useUpdateImageProfileMutation } = profileApiSlice;
