a878d4f4a113c092b4c2141d2625b9f81c0bc6ab
chourafiDev <abdelmonaimechourafi@gmail.com>
2024-02-21 17:57:56 +0100

    refactor: handle bookign checkout and get bookings

---
+++ src/store/services/bookingApiSlice.js
@@ -0,0 +1,35 @@
+import { apiSlice } from "./apiSlice";
+
+const BOOKING_URL = "booking";
+
+const apiSliceWithTag = apiSlice.enhanceEndpoints({
+  addTagTypes: ["booking"],
+});
+
+export const bookingApiSlice = apiSliceWithTag.injectEndpoints({
+  endpoints: (builder) => ({
+    getBookings: builder.query({
+      query: () => {
+        return {
+          url: `${BOOKING_URL}`,
+          method: "GET",
+          credentials: "include",
+        };
+      },
+      providesTags: ["booking"],
+    }),
+    bookingCheckOut: builder.mutation({
+      query: (data) => {
+        return {
+          url: `${BOOKING_URL}/checkout`,
+          method: "POST",
+          body: data,
+          credentials: "include",
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetBookingsQuery, useBookingCheckOutMutation } =
+  bookingApiSlice;
